f0ba5ecb470a692eaaba601f6cd0cf6424ec2a3d
code <code@danio.ir>
2024-12-26 13:05:56 +0330

    removes the redundant components from the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,37 @@
 import './App.css';
 import { useSelector } from 'react-redux';
-import { BrowserRouter, Link, Route, Routes } from 'react-router';
+import { Link, Route, Routes } from 'react-router';
 import { AppBar, Badge, Box, Button, Toolbar, Typography } from '@mui/material';
 import type { RootState } from './store/store.ts';
-import { ThemeProvider } from './theme';
 import HomePage from './pages/HomePage.tsx';
 import FavoritesPage from './pages/FavoritesPage.tsx';
 
 function App() {
   const favoriteCounts = useSelector((state: RootState) => state.recipes.favorites.length);
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" style={{ flexGrow: 1 }}>
-              Recipe Viewer
-            </Typography>
-            <Button color="inherit" component={Link} to="/">
-              Home
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Recipe Viewer
+          </Typography>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Badge badgeContent={favoriteCounts} color={'warning'}>
+            <Button color="inherit" component={Link} to="/favorites">
+              Favorites
             </Button>
-            <Badge badgeContent={favoriteCounts} color={'warning'}>
-              <Button color="inherit" component={Link} to="/favorites">
-                Favorites
-              </Button>
-            </Badge>
-          </Toolbar>
-        </AppBar>
-        <Box sx={{ padding: '10px' }}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/favorites" element={<FavoritesPage />} />
-          </Routes>
-        </Box>
-      </BrowserRouter>
-    </ThemeProvider>
+          </Badge>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ padding: '10px' }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Routes>
+      </Box>
+    </>
   );
 }
 
