c87e4610c3443d06fd6d72e4858dfb64920c16bb
code <code@danio.ir>
2024-12-26 13:04:05 +0330

    abstracting the app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router';
 import App from './App.tsx';
+import store from '~/store/store.ts';
+import { ThemeProvider } from '~/theme';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>
 );
